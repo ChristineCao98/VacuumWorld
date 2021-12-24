6f8c177e04e4f7d056239e76fe5ff36780bdc8d2
AnYiHuang <anyi.huang@ucdconnect.ie>
2021-12-24 20:42:03 +0000

    1. initialise the grid with -1s 2. use row and col to represent a location 3. print the grid and check the result (will be removed after finish developing this project)

--- src/main/java/MyModule.java
+++ src/main/java/MyModule.java
@@ -1,47 +1,68 @@
 import astra.core.Module;
+import java.util.Arrays;
 import astra.core.Agent;
 
 public class MyModule extends Module {
-    static int[][] visited=new int[16][8];
-    int lastx = -1;
-    int lasty = -1;
+    static int[][] visited=new int[8][16];
+    int lastRow = -1;
+    int lastCol = -1;
     int lastVal = 0;
+
     public void setAgent(Agent agent) {
         super.setAgent(agent);
+        // initialisation: without perceptions, all elements in the grid are seen as obstacles
+        for (int[] array : visited) {
+            Arrays.fill(array, -1);
+        }
+        printGrid();
     }
 
     @ACTION
-    public boolean updateStep(String x, String y, String id){
+    public boolean updateStep(String row, String col, String id){
         //Update the time that the square is visited
 
-        //First recover the value of the last square
-        if(lastx != -1){
-            visited[lastx][lasty] = lastVal;
-        }
-        int X = Integer.parseInt(x);
-        int Y = Integer.parseInt(y);
+        int ROW = Integer.parseInt(row);
+        int COL = Integer.parseInt(col);
         int ID = Integer.parseInt(id);
 
+        //First recover the value of the last square
+        if(lastRow != -1){
+            visited[lastRow][lastCol] = lastVal;
+        } else {
+            visited[ROW][COL] = 0;
+        }
+
         //Update the value of the current square
-        lastVal = visited[X][Y] + 1;
+        lastVal = visited[ROW][COL] + 1;
 
         //Save the value of the current square and turn it to the negative of its ID value
-        lastx = X;
-        lasty = Y;
-        visited[X][Y] = -ID;
+        lastRow = ROW;
+        lastCol = COL;
+        visited[ROW][COL] = -ID;
+        printGrid();
         return true;
     }
 
     @ACTION
-    public boolean updateObstacle(String x, String y, String squareName, String facing){
+    public boolean updateNonObstacle(String x, String y, String squareName, String facing){
         //Mark the obstacle square with -1
         return true;
     }
 
     @TERM
     public String getDirection(String x, String y){
-        //Calculate the direction that the bot should move to 
+        //Calculate the direction that the bot should move to
+        
         return "back";
+    }
+
+    public void printGrid() {
+        for (int[] row : visited) {
+            System.out.println();
+            for (int c : row) {
+                System.out.print(Integer.toString(c) + " ");
+            }
+        }
     }
 }
 
